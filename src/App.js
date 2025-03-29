fed76f7c37ebd6fe5807218d02384e68bda2e794
Masa222a <guitarstring0716@gmail.com>
2025-03-29 23:36:35 +0900

    fix css

--- src/App.js
+++ src/App.js
@@ -4,36 +4,52 @@ function App() {
   return (
     <div className="App">
       {/* 追加フォーム */}
-      <input className='add_input' type='text' placeholder='タイトル' />
-      <select className='status' name="status">
-        <option value="incomplete">未完了</option>
-        <option value="launch">着手</option>
-        <option value="complete">完了</option>
-      </select>
-      <button className='button add_button'>追加</button>
+      <div className='add_form'>
+        <input className='add_input' type='text' placeholder='タイトル' />
+        <select className='status' name="status">
+          <option value="incomplete">未完了</option>
+          <option value="launch">着手</option>
+          <option value="complete">完了</option>
+        </select>
+        <button className='button add_button'>追加</button>
+      </div>
+
+      {/* 表示切り替えのプルダウン */}
+      <div className='status_filter'>
+        <p className='filter'>フィルター</p>
+        <select className='status' name="status">
+          <option value="incomplete">未完了</option>
+          <option value="launch">着手</option>
+          <option value="complete">完了</option>
+        </select>
+      </div>
 
       {/* Todo一覧 */}
-      <ul>
-        <li>
-          <span>タイトル</span>
-          <select className='status' name="status">
-            <option value="incomplete">未完了</option>
-            <option value="launch">着手</option>
-            <option value="complete">完了</option>
-          </select>
-          <button className='button edit_button'>編集</button>
-          <button className='button delete_button'>削除</button>
-        </li>
-      </ul>
+      <div className='summary'>
+        <ul>
+          <li>
+            <span>タイトル</span>
+            <select className='status' name="status">
+              <option value="incomplete">未完了</option>
+              <option value="launch">着手</option>
+              <option value="complete">完了</option>
+            </select>
+            <button className='button edit_button'>編集</button>
+            <button className='button delete_button'>削除</button>
+          </li>
+        </ul>
+      </div>
 
       {/* 編集フォーム */}
-      <input className='edit_input' type='text' placeholder='編集する' />
-      <select className='status' name="status">
-        <option value="incomplete">未完了</option>
-        <option value="launch">着手</option>
-        <option value="complete">完了</option>
-      </select>
-      <button className='button save_button'>変更を保存する</button>
+      <div className='edit_form'>
+        <input className='edit_input' type='text' placeholder='編集する' />
+        <select className='status' name="status">
+          <option value="incomplete">未完了</option>
+          <option value="launch">着手</option>
+          <option value="complete">完了</option>
+        </select>
+        <button className='button save_button'>変更を保存する</button>
+      </div>
     </div>
   );
 }
